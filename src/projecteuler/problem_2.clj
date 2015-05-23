1e3c558e1d8fa7e95c17606d5124cee29b6093e4
Juan Ibiapina <juanibiapina@gmail.com>
2015-05-23 15:48:11 -0300

    Problem 2: make fibonacci function fully lazy

--- src/projecteuler/problem_2.clj
+++ src/projecteuler/problem_2.clj
@@ -2,7 +2,7 @@
 
 (defn fibonacci-seq
   ([] (fibonacci-seq 1 2))
-  ([a b] (cons a (lazy-seq (fibonacci-seq b (+ a b))))))
+  ([a b] (lazy-seq (cons a (fibonacci-seq b (+ a b))))))
 
 (defn problem-2 [max]
   (reduce + (filter even? (take-while #(< % max) (fibonacci-seq)))))
