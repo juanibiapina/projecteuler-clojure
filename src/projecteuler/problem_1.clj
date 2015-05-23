fbfa763feae438abee85b08009ab6555c3618646
Juan Ibiapina <juanibiapina@gmail.com>
2015-05-23 15:20:25 -0300

    Refactor problem 1 to use range

--- src/projecteuler/problem_1.clj
+++ src/projecteuler/problem_1.clj
@@ -1,14 +1,8 @@
 (ns projecteuler.problem-1)
 
-(defn divisible [n d]
+(defn divisible
+  [n d]
   (> 1 (mod n d)))
 
-(defn sum-multiples
-  [max]
-  (loop [n 1
-         sum 0]
-    (if (= n max)
-      sum
-      (if (or (divisible n 3) (divisible n 5))
-        (recur (inc n) (+ sum n))
-        (recur (inc n) sum)))))
+(defn sum-multiples [max]
+  (apply + (filter #(or (divisible % 3) (divisible % 5)) (range 1 max))))
