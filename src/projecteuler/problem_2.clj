13f9492cc978791e45645c7ff869da0f277822b9
Juan Ibiapina <juanibiapina@gmail.com>
2015-05-23 15:45:44 -0300

    Problem 2: rewrite fibonacci with multiple arities instead of helper

--- src/projecteuler/problem_2.clj
+++ src/projecteuler/problem_2.clj
@@ -1,9 +1,8 @@
 (ns projecteuler.problem-2)
 
-(defn fibonacci-seq []
-  (defn fib [a b]
-    (cons a (lazy-seq (fib b (+ a b)))))
-  (fib 1 2))
+(defn fibonacci-seq
+  ([] (fibonacci-seq 1 2))
+  ([a b] (cons a (lazy-seq (fibonacci-seq b (+ a b))))))
 
 (defn problem-2 [max]
   (reduce + (filter even? (take-while #(< % max) (fibonacci-seq)))))
